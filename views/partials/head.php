76cab30b4db61bf6501ae1b543fe3fc7c1e8c7e0
Amos <igefad12@gmail.com>
2026-04-20 17:50:11 +0100

    UPDATE: assets loading

--- views/partials/head.php
+++ views/partials/head.php
@@ -15,11 +15,20 @@
     <meta name="twitter:title" content="<?php echo $pageTitle ?? 'Savv Framework'; ?>" />
     <meta name="twitter:description" content="<?php echo $pageDescription ?? 'A lightweight PHP web framework'; ?>" />
 
-    <link rel="icon" type="image/png" href="/assets/images/icons/favicon.png">
+    <link rel="icon" type="image/png" href"<?= asset("/images/icons/favicon.png") ?>">
+    <link rel="stylesheet" href="<?= asset("/css/bootstrap-5.3.3.min.css") ?>">
+
+    <!-- CDN ALTERNATIVE -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+
+    <!-- Preconnet the links since the google-fonts.css file use the links -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
     <link
         href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,500;1,9..40,300&family=JetBrains+Mono:wght@400;500&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="<?= asset('/css/main.css') ?>">
 
     <?php if (isset($extraCSS)) echo $extraCSS; ?>
 </head>
--- views/partials/scripts.php
+++ views/partials/scripts.php
@@ -1,9 +1,11 @@
 <?php savv_scripts(); ?>
 
 <!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?= asset("/js/bootstrap-5.3.3.min.js") ?>"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> -->
 
-<script src="<?= ROOT_PATH ?>/assets/js/main.js"></script>
+
+<script src="<?= asset("/js/main.js") ?>></script>
 
 <script>
 document.getElementById('yr').textContent = new Date().getFullYear();
