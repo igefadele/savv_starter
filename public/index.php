f76d56f50a9f637646b69a1c5f2eb093ae280ce1
Amos <igefad12@gmail.com>
2026-04-21 23:30:59 +0100

    UPDATE: index.php entry

--- public/index.php
+++ public/index.php
@@ -1,10 +1,19 @@
 <?php
 
-// Setup Constants & Autoload
+// Setup Constants
+// Must define PUBLIC_PATH & ROOT_PATH
+// PUBLIC_PATH: Directory where your domain is pointed at as the Domain Document Root.
+// ROOT_PATH: Directory where the project code lives
+// You may use the same directory for both, but you must set the values of both constants separately.
+// It's recommended to use Absolute paths for these constants
+
+define('PUBLIC_PATH', __DIR__);
 define('ROOT_PATH', dirname(__DIR__));
+
+// Autoload
 require ROOT_PATH . '/vendor/autoload.php';
 
 // Bootstrap the Savv framework
-$app = \Savv\Core\Application::bootstrap(dirname(__DIR__));
+$app = \Savv\Core\Application::bootstrap(ROOT_PATH);
 
 $app->run();
